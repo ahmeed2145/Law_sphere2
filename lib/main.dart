b50fce383a9ec390b200f0619f2e6dc812ca1dab
Ahmed khaled <mazen00mazen003@gmail.com>
2025-07-15 06:44:05 +0300

    aaa
    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:law_sphere/core/di/service_locator.dart';
-import 'package:law_sphere/core/routing/app_router.dart';
-import 'package:law_sphere/low_sphere.dart';
 
+import 'core/di/service_locator.dart';
+import 'core/routing/app_router.dart';
 import 'global_bloc_observer.dart';
+import 'low_sphere.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
